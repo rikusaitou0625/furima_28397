498637ae7beb7c08995a1bde78eaf663918a94fd
rikusaitou0625 <68731892+rikusaitou0625@users.noreply.github.com>
2020-09-01 15:17:14 +0900

    レビュー修正　テストコード全角追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -73,20 +73,41 @@ RSpec.describe User, type: :model do
         @user.valid?
         expect(@user.errors.full_messages).to include("Last name can't be blank")
       end
+      it 'last_nameが全角でないと登録できない' do
+        @user.last_name = "aaa"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Last name is invalid")
+      end
       it 'first_nameが空では登録できない' do
         @user.first_name = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("First name can't be blank")
+      end
+      it 'first_nameが全角でないと登録できない' do
+        @user.first_name = "aaa"
+        @user.valid?
+        binding.pry
+        expect(@user.errors.full_messages).to include("First name is invalid")
       end
       it 'last_name_kanaが空では登録できない' do
         @user.last_name_kana = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("Last name kana can't be blank")
       end
+      it 'last_name_kanaが全角でないと登録できない' do
+        @user.last_name_kana = "aaa"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Last name kana is invalid")
+      end
       it 'first_name_kanaが空では登録できない' do
         @user.first_name_kana = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("First name kana can't be blank")
+      end
+      it 'first_name_kanaが全角でないと登録できない' do
+        @user.first_name_kana = "aaa"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First name kana is invalid")
       end
       it 'birthdayが空では登録できない' do
         @user.birthday = ''
